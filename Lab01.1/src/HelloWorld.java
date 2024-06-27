7dc887e0bb8f5efa1063100aa73b364ec083eac9
Lakea <lakeag@tlgcohort.com>
2024-06-27 11:46:32 -0400

    Initial commit

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+*
+* Classic Hello World application in Java
+* This is a standalone application, i.e., it does not run on an app server
+*/
+class HelloWorld {
+
+//    This is the starting point or entry point for every standalone Java application
+    public static void main(String[] args) {
+        System.out.println("Hello from IntelliJ"); // This prints to the console
+        System.out.printline("This is nicer than bare metal programming");
+    }
+}

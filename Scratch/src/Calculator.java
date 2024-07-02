0caa6c3844501cfa64b309acb4ed2a2f919e73c9
Lakea <lakeag@tlgcohort.com>
2024-07-02 12:26:08 -0400

    Calculator is now an "all-static" class

--- Scratch/src/Calculator.java
+++ Scratch/src/Calculator.java
@@ -1,21 +1,24 @@
+/*
+* This is an all-static class, it has nothing but static methods
+ */
 class Calculator {
 
-    double add(double a, double b) {
+    public static double add(double a, double b) {
         return a + b;
     }
 
-    double subtract(double a, double b) {
+    public static double subtract(double a, double b) {
         return a - b;
     }
 
-    boolean isEven(int value) {
+    public static boolean isEven(int value) {
         return value % 2 == 0;
     }
 
     /*
     * Returns a random integer between min and max (inclusive).
      */
-    int randomInt(int min, int max) {  // min: 5, max: 16
+    public static int randomInt(int min, int max) {  // min: 5, max: 16
         int result = 0;
 
         double rand = Math.random();    //0.0000 to 0.9999
@@ -29,14 +32,14 @@ class Calculator {
     /*
     * Returns A random integer between 1 and 11 (inclusive).
     */
-  int randomInt(int max) {
+  public static int randomInt(int max) {
       return randomInt(1, max);     // delegate to min-max version, passing 1 and max
   }
     /* HINT:see a class called Math (in package java.lang), look for a helpful method here.
     * NOTE: these methods are all "static", which means you call them as follows:
     *   Math.methodName()
      */
-    int randomInt() {
+    public static int randomInt() {
         return randomInt(1,11);     // delegate to min-max version, passing 1 for min, 11 for max
 //        int result = 0;
 //
--- Scratch/src/CalculatorClient.java
+++ Scratch/src/CalculatorClient.java
@@ -1,17 +1,16 @@
 class CalculatorClient {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
 
-        double sum = calc.add(1, 3); //passing ints where doubles are expected (implicit upcast)
+        double sum = Calculator.add(1, 3); //passing ints where doubles are expected (implicit upcast)
         System.out.println("sum is: " + sum);
 
-        System.out.println("difference is: " + calc.subtract(1.1, 3.1)); // here we pass doubles
+        System.out.println("difference is: " + Calculator.subtract(1.1, 3.1)); // here we pass doubles
 
-        System.out.println("10 is even? " + calc.isEven(10));
+        System.out.println("10 is even? " + Calculator.isEven(10));
 
         System.out.println();
 
-        System.out.println("winner is: " + calc.randomInt());
+        System.out.println("winner is: " + Calculator.randomInt(10));
     }
 }

7b62e8e3f2ddf1c7976656c56a90ab035a2346f2
Lakea <lakeag@tlgcohort.com>
2024-07-02 15:41:39 -0400

    Updated Television without red lines

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,55 +1,94 @@
 /*
-*Application or system class to model the workings of a television.
-*It has properties/attributes, it has business methods, but NO main() method.
-*/
+ * Application or system class model the workings of a television
+ * it has properties/attributes and business methods but no main() method
+ */
 class Television {
-    // class-level "static" variable - these live in the "shared-area
-    //properties or attributes - "fields" or "instance variables"
-    String brand = "Samsung"; //Default volume when not specified
-    int volume = 15;
+    // class-level "static" variables - these live in the "shared area" above the instances, ALL_CAP naming
+    public static final int MIN_VOLUME = 0;
+    public static final int MAX_VOLUME = 100;
+    private static int instanceCount = 0;
 
-    // constructor - these get turned on when the client says new
-    public Television(String brand, int volume) {
+    public static int getInstanceCount() {
+        // boolean isConnected = verifyInternetConnection();
+        // ^^^ cannot be called here bc static methods can't directly call instance methods
+        // w/o having a reference to a specific instance (object)
+        return instanceCount;
+    }
+    // ****************************************************************************
+
+    // properties or attributes - "fields" or "instance variables"
+    // default values when values are not specified
+    private String brand = "Toshiba";
+    private int volume = 1;
+
+    // constructors
+    public Television() {
+        instanceCount++;
+    }
+
+    public Television(String brand) {
         this();
         setBrand(brand);
+    }
+
+    public Television(String brand, int volume) {
+        this(brand);
         setVolume(volume);
-
     }
 
-    // functions or operations - "methods"
+    // methods
+
     public void turnOn() {
-        boolean isConnected = connectToInternet();
-        System.out.println("Turning on you " +brand + " television and setting volume to " + volume);
+        // call private method for this task
+        boolean isConnected = verifyInternetConnection();
+        // System.out.println(verifyInternetConnection());
+        System.out.println();
+
+
+        System.out.println("Turning on your " + getBrand() + " television to volume " + getVolume());
     }
+
     public void turnOff() {
-        System.out.println("Shutting down...goodbye");
+        System.out.println("Turning off... goodbye!");
     }
 
-    private boolean connectToInternet() {
-        return true;
-    }
-
-    // accessor methods - getters, setters, toString
+    //accessor methods
     public String getBrand() {
         return brand;
     }
 
-
     public void setBrand(String brand) {
+        //TODO
+        // validate brand, samdung, lg, sony, toshiba only valid brands
+        // you can use a switch to test value
         this.brand = brand;
     }
 
     public int getVolume() {
-
-        return volume; }
-
-    public String toString() {
-        return "Television: " +
-                "Brand = " + brand +
-                ", Volume = " + volume;
+        return volume;
     }
 
-    public void setVolume(int i) {
+    public void setVolume(int volume) {
+        //TODO if greater than or equal to 0 && less than or equal to 100
+        // this.volume = volume
+        // else error message
+        this.volume = volume;
+    }
 
+    /*TODO muting behavior television class
+     * muting sets volume to 0
+     * unmuting returns volume to what it was before muting
+     * fields: isMuted holds muted state,  oldVolume restores volume when unmuting
+     * needs a getter method
+     */
+
+
+    private boolean verifyInternetConnection() {
+        return true; // fake implementation
+    }
+
+    //toString()
+    public String toString() {
+        return "Television: brand=" + getBrand() + ", volume=" + getVolume();
     }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -1,42 +1,35 @@
 /*
-*Application main-class
-* in the main() method, we will create a few Televison objects and give them a basic test drive
+ * Application main class
+ * In the main() method we will create a few tv objects and give them a test drive
  */
 class TelevisionClient {
-
+    //entry point
     public static void main(String[] args) {
-        // create an instance of television and set its properties
-        Television tv1 = new Television();
-        tv1.setBrand("Samsung");
-        tv1.setVolume(32);
+        System.out.println(Television.getInstanceCount() + " instances");
+        //instance of tv, set its properties
+        Television tv1 = new Television("Samsung", 32);
 
-        // create a second instance of Television and set its properties also
-        Television tv2 = new Television();
-        tv2.setBrand("Sony");
-        tv2.setVolume(50);
+        Television tv2 = new Television("Sony", Television.MAX_VOLUME);
 
-        //Create a 3rd television object but DON'T set its propeties
-        Television tv3 = new Television();
+        //3rd tv object
+        Television tv3 = new Television("LG", Television.MIN_VOLUME);
+
 
         tv1.turnOn();
         tv1.turnOff();
-        // tv1.connectedToInternet():
-        System.out.println(tv1.toString()); //automatically called
-        System.out.println();
-
-
+        // tv1.verifyInternetConnection(); // private method in Television, no access
         tv2.turnOn();
         tv2.turnOff();
-        System.out.println(tv2);
-        System.out.println();
-
-
         tv3.turnOn();
         tv3.turnOff();
-        System.out.println(tv3);
+
         System.out.println();
 
+        // calling the toString()
+        System.out.println(tv1.toString());
+        System.out.println(tv2);
+        System.out.println(tv3);
 
-
+        System.out.println(Television.getInstanceCount() + " instances");
     }
 }

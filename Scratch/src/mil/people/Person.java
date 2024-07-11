3b5f6a746e6ffdac63d83422504968c57aa5cdc7
Lakea <lakeag@tlgcohort.com>
2024-07-11 10:21:13 -0400

    Fixed package name

--- Scratch/src/mil/people/Person.java
+++ Scratch/src/mil/people/Person.java
@@ -1,13 +1,15 @@
-package mil.people;/*
-* An immutable class.
-* That is, a class definition written in such a way that instance of it (Person objects),
-* once created, cannot have their own properties changed.
-* There are simply no public methods to do so, i.e., getters, but NO setters
- */
+package mil.people;
 
 import java.time.LocalDate;
 import java.time.Period;
 
+/*
+ * An immutable class.
+ *
+ * That is, a class definition written in such a way that instance of it (Person objects),
+ * once created, cannot have their properties changed.
+ * There are simply no public methods to do so, i.e., it has getters, but NO setters.
+ */
 class Person {
     private final String name;
     private final LocalDate birthDate;
@@ -16,18 +18,17 @@ class Person {
         this.name = name;
         this.birthDate = birthDate;
     }
+
     /*
-    *Returns the person's name in whole years.
-    * This is a period of time between the birthdate and today's date
-    * This is a "derived" property, it's calculated from existing data, not a new field.
+     * Returns the person's age in whole years.
+     * This is the period of time between the birthdate and today's date.
+     * This is a "derived" property, i.e., it is "calculated" from existing data, not a new field.
+     *
+     * CUSTOMER QUESTION; what do we return if this value is < 1? (e.g., 8 months)
      */
-    public int currentAge() {
-        LocalDate currentDate = LocalDate.now();
-        Period period = Period.between(birthDate, currentDate);
-        return period.getYears();
+    public int age() {
+        return Period.between(birthDate(), LocalDate.now()).getYears();
     }
-
-
 
     public String name() {
         return name;
--- Scratch/src/mil/people/PersonClient.java
+++ Scratch/src/mil/people/PersonClient.java
@@ -3,10 +3,13 @@ package mil.people;
 import java.time.LocalDate;
 
 class PersonClient {
+
     public static void main(String[] args) {
         Person p1 = new Person("Nova", LocalDate.of(2024, 03, 18));
 
-        System.out.printf("%s is %s years old\n", p1.name(), p1.currentAge());
-        System.out.println(p1);
+        // print the following: <name> is <age> years old
+        System.out.printf("%s is %s years old\n", p1.name(), p1.age());
+
+        System.out.println(p1);  // toString() automatically called
     }
 }
